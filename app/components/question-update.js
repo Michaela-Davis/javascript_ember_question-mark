6779e8f0140868652ce076140b0fe9aaa827e539
Michaela-Davis <michaela.delight@gmail.com>
2017-03-25 14:11:26 -0700

    add ability to update questions on question page

---
+++ app/components/question-update.js
@@ -0,0 +1,20 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  updateQuestionForm: false,
+  actions: {
+    updateQuestionForm(question) {
+      this.set('updateQuestionForm', true);
+    },
+
+    update(question) {
+      var params = {
+        title: this.get('title'),
+        notes: this.get('notes'),
+        author: this.get('author'),
+      };
+      this.set('updateQuestionForm', false);
+      this.sendAction('update', question, params);
+    }
+  }
+});
