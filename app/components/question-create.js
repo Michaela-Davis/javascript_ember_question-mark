c795b4b2f1e69cc4627d796bf83d15b905db94e5
Michaela-Davis <michaela.delight@gmail.com>
2017-03-24 19:53:20 -0700

    add ability to create new questions

--- app/components/question-create.js
+++ app/components/question-create.js
@@ -4,6 +4,16 @@ export default Ember.Component.extend({
   actions: {
     createNewQuestion() {
       this.set('createNewQuestion', true);
+    },
+
+    saveQuestion() {
+      var params = {
+        title: this.get('title'),
+        notes: this.get('notes'),
+        author: this.get('author'),
+      };
+      this.set('createNewQuestion', false);
+      this.sendAction('saveQuestion', params);
     }
   }
 });
